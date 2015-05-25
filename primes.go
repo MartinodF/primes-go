5c70294a604be78964756ae402bf76a458da08b1
Martino di Filippo <puntodifuga@gmail.com>
2015-05-25 18:22:16 +0200

    Split main() into cmd.go

--- primes.go
+++ primes.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-  "flag"
   "fmt"
   "io"
-  "os"
-  "path/filepath"
-  "time"
 )
 
 func allocate(limit uint64) []uint64 {
@@ -63,50 +59,9 @@ func sieve(limit uint64, primes []uint64) {
 func saveTo(w io.Writer, primes []uint64) {
   for k, p := range primes {
     for l := uint8(0); l < 64; l++ {
-      if (p >> l) & 1 == 0 {
-        fmt.Fprintln(w, (k * 64 + int(l)) * 2 + 3)
+      if (p>>l)&1 == 0 {
+        fmt.Fprintln(w, (k*64+int(l))*2+3)
       }
     }
-  }
-}
-
-func main() {
-  limit := flag.Uint64("l", 100000, "Upper limit for prime searching")
-  write := flag.Bool("s", false, "Save the prime numbers to primes.txt")
-
-  flag.Usage = func() {
-    fmt.Fprintln(os.Stderr, "A quick test implementing the Sieve of Eratostenes in Go")
-    fmt.Fprintln(os.Stderr, "It searches for prime numbers up to the specified limit\n")
-    fmt.Fprintf(os.Stderr, "Usage: %s [-l limit] [-s]\n\n", filepath.Base(os.Args[0]))
-    flag.PrintDefaults()
-    fmt.Fprint(os.Stderr, "\n")
-  }
-
-  flag.Parse()
-
-  primes := allocate(*limit)
-
-  fmt.Printf("Finding all primes <= %v\n", *limit)
-  start := time.Now()
-
-  sieve(*limit, primes)
-
-  fmt.Printf("Done in %v.\n", time.Since(start))
-
-  if *write {
-    file, err := os.Create("primes.txt")
-    if err != nil { panic(err) }
-    defer file.Close()
-
-    fmt.Printf("\nSaving to primes.txt...\n");
-    start := time.Now()
-
-    if *limit >= 2 {
-      fmt.Fprintln(file, 2)
-    }
-
-    saveTo(file, primes)
-
-    fmt.Printf("Done in %v.\n", time.Since(start))
   }
 }
